6fb4e726de432670ef07ad3602de04a60b94f9f7
hilmywashil <hilmywashil@gmail.com>
2025-05-10 14:36:11 +0700

    Update Portfolio component to include a direct link to the GitHub repository for the school website project and remove the placeholder site link.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -13,8 +13,7 @@ const projects = [
     title: "SCHOOL WEBSITE USING LARAVEL",
     description: "This is a school website that I made using Laravel. It is a simple website that allows you to view the school's information and the students' information.",
     links: {
-      site: "#",
-      github: "#",
+      github: "https://github.com/hilmywashil/smk-it-baitul-aziz",
     },
   },
   // {
@@ -74,17 +73,11 @@ const Portfolio = () => {
               <h3 className='text-2xl font-semibold text-gray-200 mb-4'>{project.title}</h3>
               <p className='text-gray-300 mb-4'>{project.description}</p>
               <div className='flex space-x-4'>
-                <a href={project.links.site}
-                  className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700
-                                      transition duration-300'>
-                  View Site
-                </a>
                 <a href={project.links.github}
                   className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700
-                                      transition duration-300'>
-                  <AiOutlineGithub />
+                                      transition duration-300 flex items-center gap-2'>
+                  <AiFillGithub /> View on Github
                 </a>
-
               </div>
 
             </div>
