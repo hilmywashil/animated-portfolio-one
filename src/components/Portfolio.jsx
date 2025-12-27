fb0dcfc6ca8042d6e2b1724482690c495bef2e5d
Hilmy Washil <hilmywashil@gmail.com>
2025-12-27 16:23:32 +0700

    Update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import project1 from "../assets/school-web.png"
+import project1 from "../assets/project1.png"
 import project2 from "../assets/project2.png"
+import project3 from "../assets/project3.png"
 import project4 from "../assets/project4.png"
 import project5 from "../assets/project5.png"
 import project6 from "../assets/project6.png"
@@ -10,15 +11,16 @@ import Reveal from './Reveal';
 const projects = [
   {
     img: project1,
-    title: "SCHOOL WEBSITE USING LARAVEL",
-    description: "This is a school website that I made using Laravel. It is a simple website that allows you to view the school's information and the students' information.",
+    title: "Lontar Digital",
+    description: "",
     links: {
+      site: "https://lontardigital.com",
       github: "https://github.com/hilmywashil/smk-it-baitul-aziz",
     },
   },
   {
     img: project2,
-    title: "Project #2",
+    title: "Gunarso Gunadi Dental",
     description: "A fullstack application built with Node.js and MongoDB.",
     links: {
       site: "#",
@@ -26,7 +28,7 @@ const projects = [
     },
   },
   {
-    img: project4,
+    img: project3,
     title: "Project #3",
     description: "An e-commerce platform with various features.",
     links: {
@@ -34,24 +36,24 @@ const projects = [
       github: "#",
     },
   },
-  // {
-  //   img: project5,
-  //   title: "Project #4",
-  //   description: "A mobile-friendly application using React Native.",
-  //   links: {
-  //     site: "#",
-  //     github: "#",
-  //   },
-  // },
-  // {
-  //   img: project6,
-  //   title: "Project #5",
-  //   description: "A data visualization project using D3.js and other libraries.",
-  //   links: {
-  //     site: "#",
-  //     github: "#",
-  //   },
-  // },
+  {
+    img: project4,
+    title: "Project #4",
+    description: "A mobile-friendly application using React Native.",
+    links: {
+      site: "#",
+      github: "#",
+    },
+  },
+  {
+    img: project5,
+    title: "Project #5",
+    description: "A data visualization project using D3.js and other libraries.",
+    links: {
+      site: "#",
+      github: "#",
+    },
+  },
 ]
 
 const Portfolio = () => {
@@ -73,11 +75,16 @@ const Portfolio = () => {
               <h3 className='text-2xl font-semibold text-gray-200 mb-4'>{project.title}</h3>
               <p className='text-gray-300 mb-4'>{project.description}</p>
               <div className='flex space-x-4'>
-                <a href={project.links.github}
+                <a href={project.links.site}
+                  className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700
+                                        transition duration-300'>
+                  View Site
+                </a>
+                {/* <a href={project.links.github}
                   className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700
                                       transition duration-300 flex items-center gap-2'>
                   <AiFillGithub /> View on Github
-                </a>
+                </a> */}
               </div>
 
             </div>
